be41f1aa0ad345ee097c14c7ad7d3d07f008a558
John Washbourne <jwashbourne@chevron.com>
2021-09-21 21:24:01 +0000

    Added 3d q setup methods

--- src/absorb.jl
+++ src/absorb.jl
@@ -452,9 +452,7 @@ function absorb(c33::Array{T,3}, den::Array{T,3}, nz::Int64, ny::Int64, nx::Int6
     return abc
 end
 
-# TODO: 3D equivalent operators
-# TODO: timing for threaded ops in 3D 
-# TODO: should we use LoopVectorization.jl ?
+# TODO: investigate LoopVectorization.jl for performance?
 
 function setup_q_profile_2D_serial!(w_inv_q::Array{T,2}, freesurface::Bool, nsponge::Int64, 
         dt::T, q_freq::T, q_min::T, q_interior::T) where T<:Float32
@@ -471,7 +469,6 @@ function setup_q_profile_2D_serial!(w_inv_q::Array{T,2}, freesurface::Bool, nspo
 
     for kz = 1:nz
         ksz = (freesurface) ? (nz - 1 - (kz - 1)) : min((kz - 1), (nz - 1 - (kz - 1)))
-        
         for kx = 1:nx
             ksx = min((kx - 1),   (nx - 1 - (kx - 1)))
             ksponge = min(ksx, ksz) + 1 # add one to get back to julia indexing
@@ -483,30 +480,28 @@ function setup_q_profile_2D_serial!(w_inv_q::Array{T,2}, freesurface::Bool, nspo
     end
 end
 
-function setup_q_profile_2D_threaded!(w_inv_q::Array{T,2}, freesurface, nsponge::Int64, 
-        dt::T, q_freq::T, q_min::T, q_interior::T) where T<:Float32
+function make_qprof(nsponge::Int64, q_min::T, q_interior::T) where T<:Float32
+    lqmin = log(q_min)
+    lqmax = log(q_interior)
+    qprof = zeros(Float32, nsponge)
 
-    function make_qprof(nsponge::Int64, q_min::T, q_interior::T) where T<:Float32
-        lqmin = log(q_min)
-        lqmax = log(q_interior)
-        qprof = zeros(Float32, nsponge)
-
-        @threads for ksponge = 1:nsponge
-            dk = (ksponge - 1) / (nsponge - 1)
-            lq = lqmin + dk * (lqmax - lqmin)
-            @inbounds qprof[ksponge] = exp(lq)
-        end
-
-        return qprof
+    @threads for ksponge = 1:nsponge
+        dk = (ksponge - 1) / (nsponge - 1)
+        lq = lqmin + dk * (lqmax - lqmin)
+        @inbounds qprof[ksponge] = exp(lq)
     end
 
-    function compute_w_inv_q!(w_inv_q::Array{T,2}, qprof::Array{T,1}, freesurface::Bool, nsponge::Int64, q_freq::T, q_interior::T) where T<:Float32
+    return qprof
+end
+
+function setup_q_profile_2D_threaded!(w_inv_q::Array{T,2}, freesurface, nsponge::Int64, 
+        dt::T, q_freq::T, q_min::T, q_interior::T) where T<:Float32
+    
+    function compute_w_inv_q_2D!(w_inv_q::Array{T,2}, qprof::Array{T,1}, freesurface::Bool, nsponge::Int64, q_freq::T, q_interior::T) where T<:Float32
         nz,nx = size(w_inv_q)
-        two_pi_f = T(2.0 * π * q_freq)
 
         @threads for kz = 1:nz
             ksz = (freesurface) ? (nz - 1 - (kz - 1)) : min((kz - 1), (nz - 1 - (kz - 1)))
-            
             for kx = 1:nx
                 ksx = min((kx - 1),   (nx - 1 - (kx - 1)))
                 ksponge = min(ksx, ksz) + 1 # add one to get back to julia indexing
@@ -519,5 +514,62 @@ function setup_q_profile_2D_threaded!(w_inv_q::Array{T,2}, freesurface, nsponge:
     end
 
     qprof = make_qprof(nsponge, q_min, q_interior)
-    compute_w_inv_q!(w_inv_q, qprof, freesurface, nsponge, q_freq, q_interior)
+    compute_w_inv_q_2D!(w_inv_q, qprof, freesurface, nsponge, q_freq, q_interior)
+end
+
+function setup_q_profile_3D_serial!(w_inv_q::Array{T,3}, freesurface::Bool, nsponge::Int64, 
+    dt::T, q_freq::T, q_min::T, q_interior::T) where T<:Float32
+    nz,ny,nx = size(w_inv_q)
+    lqmin = log(q_min)
+    lqmax = log(q_interior)
+    qprof = zeros(Float32, nsponge)
+
+    for ksponge = 1:nsponge
+        dk = (ksponge - 1) / (nsponge - 1)
+        lq = lqmin + dk * (lqmax - lqmin)
+        qprof[ksponge] = T(exp(lq))
+    end
+
+    for kz = 1:nz
+        ksz = (freesurface) ? (nz - 1 - (kz - 1)) : min((kz - 1), (nz - 1 - (kz - 1)))
+        
+        for ky = 1:ny
+            ksy = min((ky - 1),   (ny - 1 - (ky - 1)))
+            for kx = 1:nx
+                ksx = min((kx - 1),   (nx - 1 - (kx - 1)))
+                ksponge = min(ksx,min(ksy, ksz)) + 1 # add one to get back to julia indexing
+                @inbounds w_inv_q[kz,ky,kx] = (dt * 2.0 * π * q_freq / q_interior)
+                if ksponge ∈ 1:nsponge
+                    @inbounds w_inv_q[kz,ky,kx] = dt * 2.0 * π * q_freq / qprof[ksponge]
+                end
+            end
+        end
+    end
+end
+
+function setup_q_profile_3D_threaded!(w_inv_q::Array{T,3}, freesurface, nsponge::Int64, 
+    dt::T, q_freq::T, q_min::T, q_interior::T) where T<:Float32
+
+    function compute_w_inv_q_3D!(w_inv_q::Array{T,3}, qprof::Array{T,1}, freesurface::Bool, nsponge::Int64, q_freq::T, q_interior::T) where T<:Float32
+        nz,ny,nx = size(w_inv_q)
+
+        @threads for kz = 1:nz
+            ksz = (freesurface) ? (nz - 1 - (kz - 1)) : min((kz - 1), (nz - 1 - (kz - 1)))
+            
+            for ky = 1:ny
+                ksy = min((ky - 1),   (ny - 1 - (ky - 1)))
+                for kx = 1:nx
+                    ksx = min((kx - 1),   (nx - 1 - (kx - 1)))
+                    ksponge = min(ksx,min(ksy, ksz)) + 1 # add one to get back to julia indexing
+                    @inbounds w_inv_q[kz,ky,kx] = dt * 2.0 * π * q_freq / q_interior
+                    if ksponge ∈ 1:nsponge
+                        @inbounds w_inv_q[kz,ky,kx] = dt * 2.0 * π * q_freq / qprof[ksponge]
+                    end
+                end
+            end
+        end
+    end
+
+    qprof = make_qprof(nsponge, q_min, q_interior)
+    compute_w_inv_q_3D!(w_inv_q, qprof, freesurface, nsponge, q_freq, q_interior)
 end
--- test/absorb.jl
+++ test/absorb.jl
@@ -1,8 +1,8 @@
 using  Random, Test, WaveFD, LinearAlgebra
 
 nthreads = 12
-nz,nx = 201, 401
-dz,dx = 25.0f0, 25.0f0
+nz,ny,nx = 101, 200, 301
+dz,dy,dx = 25.0f0,25.0f0, 25.0f0
 nsponge = 40
 freqQ = 5.0f0
 qMin = 0.1f0
@@ -34,6 +34,34 @@ end
     w2 = zeros(Float32,nz,nx)
     WaveFD.setup_q_profile_2D_serial!(w1, freesurface, nsponge, dtmod, freqQ, qMin, qInterior)
     WaveFD.setup_q_profile_2D_threaded!(w2, freesurface, nsponge, dtmod, freqQ, qMin, qInterior)
+    @test w1 ≈ w2
+end
+
+@testset "3D, serial, freesurface=$(freesurface)" for freesurface in (false,true)
+    w1 = zeros(Float32,nz,ny,nx)
+    w2 = zeros(Float32,nz,ny,nx)
+    WaveFD.setup_q_profile_3D_serial!(w1, freesurface, nsponge, dtmod, freqQ, qMin, qInterior)
+    w2 .= WaveFD.DtOmegaInvQ(Prop3DAcoIsoDenQ_DEO2_FDTD(;
+        freesurface=freesurface, nz=nz, ny=ny, nx=nx, dz=dz, dy=dy, dx=dx, nsponge=nsponge, nthreads=nthreads,
+        dt=dtmod, freqQ=freqQ, qMin=qMin, qInterior=qInterior))
+    @test w1 ≈ w2
+end
+
+@testset "3D, threaded, freesurface=$(freesurface)" for freesurface in (false,true)
+    w1 = zeros(Float32,nz,ny,nx)
+    w2 = zeros(Float32,nz,ny,nx)
+    WaveFD.setup_q_profile_3D_threaded!(w1, freesurface, nsponge, dtmod, freqQ, qMin, qInterior)
+    w2 .= WaveFD.DtOmegaInvQ(Prop3DAcoIsoDenQ_DEO2_FDTD(;
+        freesurface=freesurface, nz=nz, ny=ny, nx=nx, dz=dz, dy=dy, dx=dx, nsponge=nsponge, nthreads=nthreads,
+        dt=dtmod, freqQ=freqQ, qMin=qMin, qInterior=qInterior))
+    @test w1 ≈ w2
+end
+
+@testset "3D, serial == threaded, freesurface=$(freesurface)" for freesurface in (false,true)
+    w1 = zeros(Float32,nz,ny,nx)
+    w2 = zeros(Float32,nz,ny,nx)
+    WaveFD.setup_q_profile_3D_serial!(w1, freesurface, nsponge, dtmod, freqQ, qMin, qInterior)
+    WaveFD.setup_q_profile_3D_threaded!(w2, freesurface, nsponge, dtmod, freqQ, qMin, qInterior)
     @test w1 ≈ w2
 end
 
