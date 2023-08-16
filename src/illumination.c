dcf7d8af574fccc8567fe4a62f4f7dd589360f16
samkaplan <sam.kaplan@chevron.com>
2023-08-16 13:37:03 -0500

    time taper for src illum

--- src/illumination.c
+++ src/illumination.c
@@ -3,25 +3,27 @@
 void
 srcillum_helper_float(
     float* field_ginsu,
+    float  time_mask,
     size_t n,
     int    nthreads)
 {
     size_t i;
 #pragma omp parallel for num_threads(nthreads)
     for (i = 0; i < n; i++) {
-        field_ginsu[i] = field_ginsu[i]*field_ginsu[i];
+        field_ginsu[i] = time_mask*field_ginsu[i]*field_ginsu[i];
     }
 }
 void
 srcillum_helper_double(
     double* field_ginsu,
+    double  time_mask,
     size_t  n,
     int     nthreads)
 {
     size_t i;
 #pragma omp parallel for num_threads(nthreads)
     for (i = 0; i < n; i++) {
-        field_ginsu[i] = field_ginsu[i]*field_ginsu[i];
+        field_ginsu[i] = time_mask*field_ginsu[i]*field_ginsu[i];
     }
 }
 
@@ -29,25 +31,27 @@ void
 illum_accumulate_float(
     float* field_ginsu_accum,
     float* field_ginsu,
+    float  time_mask,
     size_t n,
     int    nthreads)
 {
     size_t i;
 #pragma omp parallel for num_threads(nthreads)
     for (i = 0; i < n; i++) {
-        field_ginsu_accum[i] += field_ginsu[i]*field_ginsu[i];
+        field_ginsu_accum[i] += time_mask*field_ginsu[i]*field_ginsu[i];
     }
 }
 void
 illum_accumulate_double(
     double* field_ginsu_accum,
     double* field_ginsu,
+    double  time_mask,
     size_t n,
     int    nthreads)
 {
     size_t i;
 #pragma omp parallel for num_threads(nthreads)
     for (i = 0; i < n; i++) {
-        field_ginsu_accum[i] += field_ginsu[i]*field_ginsu[i];
+        field_ginsu_accum[i] += time_mask*field_ginsu[i]*field_ginsu[i];
     }
 }
